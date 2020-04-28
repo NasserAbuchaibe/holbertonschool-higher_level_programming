bae0c2d9aef2cab36dbf77cc48e67bd21705e23c
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-04-28 18:42:51 +0000

    7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-	alf = ord(c)
-	if alf >= 97 and alf <= 112:
-		return True
-	else:
-		return False
+    alf = ord(c)
+    if alf >= 97 and alf <= 112:
+        return True
+    else:
+        return False
