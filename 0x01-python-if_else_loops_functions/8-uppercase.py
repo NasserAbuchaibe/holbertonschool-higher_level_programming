6dcaf472bffc241bc7751bf86f502451ce574f4a
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-04-28 18:48:00 +0000

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 def uppercase(str):
-	for alf in range(len(str)):
-		aux = ord(str[alf])
-		if aux >= 97 and aux <= 122:
-			aux -=32
-		print("{:c}".format(aux), end = "")
-	print()
+    for alf in range(len(str)):
+        aux = ord(str[alf])
+        if aux >= 97 and aux <= 122:
+            aux-=32
+    print("{:c}".format(aux), end="")
+    print()
