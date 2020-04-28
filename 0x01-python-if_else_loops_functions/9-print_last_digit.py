67f9dc78d4fc7f6fea630f515728868015032662
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-04-28 19:09:32 +0000

    update

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+
+
 def print_last_digit(number):
-	if number < 0:
-		number*= (-1)
-	print("{:d}".format(number % 10), end = "")
-	return number % 10
+    if number < 0:
+        number *= (-1)
+    print("{:d}".format(number % 10), end="")
+    return number % 10
