b9aaec7a633a4be522381cc4bba502c356b44b39
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-04-28 20:58:13 +0000

    FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for x in range(1, 101):
-        if x % 3 == 0 and x % 5 == 0:
-            print("FizzBuzz ", end="")
+        if (x % 3 == 0) and (x % 5 == 0):
+            print("{} ".format("FizzBuzz"), end="")
         elif x % 3 == 0:
-            print("Fizz ", end="")
+            print("{} ".format("Fizz"), end="")
         elif x % 5 == 0:
-            print("Buzz ", end="")
+            print("{} ".format("Buzz"), end="")
         else:
-            print(x, " ", end="")
+            print("{:d} ".format(x), end="")
