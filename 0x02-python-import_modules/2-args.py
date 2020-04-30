93843eaa9e5e1290bae98d6dde35fae52e9e2571
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-04-30 18:31:55 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    size = len(sys.argv)
+    if size == 1:
+        print("{:d} arguments.".format(size - 1))
+    elif size == 2:
+        print("{:d} argument:".format(size - 1))
+    else:
+        print("{:d} arguments:".format(size - 1))
+for x in range(1, size):
+    print("{:d}: {:s}".format(x, sys.argv[x]))
