646918e25e2407d8b5f59a225b7550f920dc289d
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-04-30 20:46:47 +0000

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
             print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-                exit(1)
+            exit(1)
     else:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
