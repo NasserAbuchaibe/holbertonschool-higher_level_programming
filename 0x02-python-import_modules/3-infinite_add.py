6894593a0c7228d6590f903378377f70fe957c11
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-04-30 19:10:10 +0000

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+sum1 = 0
+if __name__ == "__main__":
+    import sys
+    size = len(sys.argv)
+    for x in range(1, size):
+        sum1 = sum1 + int(sys.argv[x])
+print("{}".format(sum1))
