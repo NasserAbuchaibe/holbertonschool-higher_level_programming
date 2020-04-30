7c8814191f53fdab6220592bd516a6bc6e4e1ea4
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-04-30 20:34:17 +0000

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if len(sys.argv) == 4:
+    if 4 == len(sys.argv):
         if sys.argv[2] == '+':
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
         elif sys.argv[2] == '-':
