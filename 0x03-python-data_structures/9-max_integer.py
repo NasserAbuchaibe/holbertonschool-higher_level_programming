701de8f0885c627ee33ca2bba35b79ea7bddf4cb
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-05-05 22:57:14 +0000

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_n = 0
+    if len(my_list) == 0:
+        return None
+    for i in my_list:
+        if max_n < i:
+            max_n = i
+    return max_n
