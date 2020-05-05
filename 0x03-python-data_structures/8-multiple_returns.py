86857317a394889bfb462800440a7c53816d9367
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-05-05 22:31:08 +0000

    fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     size = len(sentence)
-    alpha = sentence[0]
+    if len(sentence) == 0:
+        alpha = None
+    else:
+        alpha = sentence[0]
     return size, alpha
