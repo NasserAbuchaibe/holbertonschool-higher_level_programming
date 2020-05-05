25bd10620c29edf2f76030d473fdba02eff3c63c
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-05-05 22:58:47 +0000

    fix

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     max_n = 0
     if len(my_list) == 0:
-        return None
+        return "None"
     for i in my_list:
         if max_n < i:
             max_n = i
