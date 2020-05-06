88ec467c3ccf49ced6a25e89b6519654a37424ab
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-05-06 00:45:15 +0000

    fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ''
     for i in my_string:
-        if 'c' != i or 'C' != i:
+        if 'c' != i and 'C' != i:
             new_string += i
     return new_string 
