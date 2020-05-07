3b53048fbc786ba37f8d356202cdb748f0452f83
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-05-07 17:23:18 +0000

    fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-def pow_(num):
-    return num * num
-
 def square_matrix_simple(matrix=[]):
-    matrix_n = []
-    for i in matrix:
-            matrix_n.append(list(map(pow_, i)))
-    return matrix_n
+    if not matrix:
+        print()
+    return [[i ** 2 for i in x] for x in matrix]
