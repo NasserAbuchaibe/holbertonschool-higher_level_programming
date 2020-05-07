1a5580b85f08eb34d6105dbf1c2ea4080d568678
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-05-07 18:05:53 +0000

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        return max(a_dictionary)
+    max_ = 0
+    if not a_dictionary:
+        return "None"
+    for k, v in a_dictionary.items():
+        if max_ < v:
+            max_ = v
+    for k, v in a_dictionary.items():
+        if max_ == v:
+            return k
