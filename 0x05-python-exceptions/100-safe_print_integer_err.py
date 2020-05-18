8fa391cef3aced80501ae17ffc7593363cd9c1d4
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-05-18 19:19:06 +0000

    fix stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception as mo:
-        print("Exception: {}".format(mo))
+    except ValueError as error:
+        sys.stderr.write("Exception: {}\n".format(error))
         return False
