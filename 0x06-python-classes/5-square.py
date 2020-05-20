e411bb273fe869408842e71b03af2fbc8642a11f
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-05-20 01:09:01 +0000

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+"""5-square.py: Class Square"""
+
+
+class Square:
+    """Class  Square: initialize the size attribute """
+    def __init__(self, size=0):
+        """
+        initialize the square class with size
+        Args:
+        size: size of square
+        Return:
+        None
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._Square__size = size
+
+    @property
+    def size(self):
+        """ return size of square """
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        """ set size square """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._Square__size = value
+
+    def area(self):
+        """ Calculate and return the area of the square """
+        return self._Square__size ** 2
+
+    def my_print(self):
+        """ print a square """
+        size = self._Square__size
+        if size == 0:
+            print()
+        else:
+            for i in range(size):
+                print("#" * size)
