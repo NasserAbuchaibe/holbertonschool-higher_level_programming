c182b017eba843899f3cb3ceabd5fa26910c8db2
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-05-23 15:23:46 +0000

    fix cas2

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -19,14 +19,11 @@ def print_square(size):
     if (type(size) == float and size < 0):
         raise TypeError("size must be an integer")
 
-    if type(size) != int:
+    elif type(size) != int:
         raise TypeError("size must be an integer")
 
-    if size < 0:
+    elif size < 0:
         raise ValueError("size must be >= 0")
-
-    if size == 0:
-        print()
     else:
         for x in range(size):
             print("#" * size)
