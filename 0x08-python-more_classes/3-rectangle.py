6ebf6b1f0a84d26b745437c5b7dcb74d1a46b729
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-05-25 20:04:30 +0000

    3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
         Args:
             value (int): width rectagle
         """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -45,7 +45,7 @@ class Rectangle:
         Args:
             value (int): heigth rectagle
         """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
@@ -76,4 +76,3 @@ class Rectangle:
             else:
                 aux_str += ('#' * self.__width) + '\n'
         return aux_str
-    
