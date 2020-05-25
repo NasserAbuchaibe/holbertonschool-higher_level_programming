b7a0e0ca30f084d726e0619fddba2bfdac635e2d
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-05-25 14:01:02 +0000

    fix pep8

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-    """0-rectangle.py: Class Rectangle
+"""0-rectangle.py: Class Rectangle
+"""
+
+
+class Rectangle:
+    """Class Rectangle
     """
-    
-    
-    class Rectangle:
-        """Class Rectangle
-        """
-         pass
+    pass
