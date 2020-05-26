1601e56894fcf1ce51ae6ba3738209c0dc3672e9
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-05-26 23:34:58 +0000

    fix docstring

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""that prevents the user from dynamically creating new instance attributes,
-    except if the new instance attribute is called first_name.
-"""
+""" Module for LockedClass """
 
 
 class LockedClass:
+    """that prevents the user from dynamically creating new instance attributes,
+       except if the new instance attribute is called first_name."""
     __slots__ = ['first_name']
