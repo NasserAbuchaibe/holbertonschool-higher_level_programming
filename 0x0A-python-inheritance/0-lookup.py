6f19c19484d9c936b3574baf15013047e8b73a3b
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-06-02 17:24:25 +0000

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""0-lookup.py
+"""
+
+
+def lookup(obj):
+    """eturns the list of available attributes and methods of an objec
+
+    Args:
+        obj: Object
+    """
+    return dir(obj)
