0dd693b0a9c876219ab26a4e6d7e5a60b841d2a9
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-06-03 23:50:27 +0000

    13-student.py

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" class "Student"  """
+
+
+class Student():
+    """ class student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ returns dictionary """
+        if attrs is None:
+            return self.__dict__
+
+        my_dict = {}
+        for x in attrs:
+            if x in self.__dict__:
+                my_dict[x] = self.__dict__[x]
+        return my_dict
+
+    def reload_from_json(self, json):
+        """ Relplaces values in class instance """
+        for key in json:
+            self.__dict__[key] = json[key]
