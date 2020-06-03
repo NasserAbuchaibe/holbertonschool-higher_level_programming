e57980506693dea98bc809525d1ea2e7b2ec21f4
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-06-03 01:13:21 +0000

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+"""BaseGeometry Class module"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Squeare class
+    Args:
+        Rectangle (class) : class to inherit
+    """
+
+    def __init__(self, size):
+        """Inizialition square Class"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
