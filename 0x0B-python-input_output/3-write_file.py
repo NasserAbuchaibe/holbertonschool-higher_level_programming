792ffddadfa550d360f95a40bf4836986cdef779
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-06-03 21:51:38 +0000

    fix style

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ 3-write_file.py """
+
+
 def write_file(filename="", text=""):
+
     """ writes a string to a text file (UTF8)
         and returns the number of characters written:
 
