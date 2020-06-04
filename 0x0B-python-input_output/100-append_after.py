cc5683ade9f8be650187b10f68f583770fcf391a
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-06-04 02:47:59 +0000

    fix

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,18 +3,12 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """[summary]
-
-    Args:
-        filename (str, optional): [description]. Defaults to "".
-        search_string (str, optional): [description]. Defaults to "".
-        new_string (str, optional): [description]. Defaults to "".
-    """
+    """[summary]"""
     with open(filename, "r+", encoding="utf-8") as f:
         list_text = f.readlines()
 
-    cont_lines = 0
     with open(filename, "w", encoding="utf-8") as f:
+        cont_lines = 0
         for line in list_text:
             cont_lines += 1
             if search_string in line:
