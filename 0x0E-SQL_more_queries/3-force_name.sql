a397e67504ac86abf739ff3fcccef24bac723426
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-07-14 21:18:24 +0000

    fix

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,2 @@
 -- Write a script that creates the table force_name.
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT, name VARCHAR(256) TEXT NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
