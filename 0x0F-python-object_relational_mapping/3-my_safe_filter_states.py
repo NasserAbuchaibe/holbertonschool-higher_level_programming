d042808fec8fdf17da48f5e357ad0390aef275f6
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-08-08 22:38:14 +0000

    fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,21 +6,18 @@ from sys import argv
 
 def filterEstates():
     """  MySQLdb. """
-    if len(argv) == 5:
-        db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
-                             passwd=argv[2], db=argv[3])
-        cur = db.cursor()
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
 
-        cur.execute('SELECT * FROM states WHERE name ="{}" ORDER\
-                    BY id ASC'.format(argv[4]))
-        states = cur.fetchall()
-        for row in states:
-            if row[1] == argv[4]:
-                print(row)
+    cur.execute('SELECT * FROM states WHERE name ="{}" ORDER\
+                BY id ASC'.format(argv[4]))
+    states = cur.fetchall()
+    for row in states:
+        if row[1] == argv[4]:
+            print(row)
 
-        db.close()
-    else:
-        pass
+    db.close()
 
 if __name__ == '__main__':
     filterEstates()
