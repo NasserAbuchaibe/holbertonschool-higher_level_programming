3f9fe7d8b4b348c89f80097bf12c936d63ed2486
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-08-08 22:33:11 +0000

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+""" Filter states """
+import MySQLdb
+from sys import argv
+
+
+def filterEstates():
+    """  MySQLdb. """
+    if len(argv) == 5:
+        db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                             passwd=argv[2], db=argv[3])
+        cur = db.cursor()
+
+        cur.execute('SELECT * FROM states WHERE name ="{}" ORDER\
+                    BY id ASC'.format(argv[4]))
+        states = cur.fetchall()
+        for row in states:
+            if row[1] == argv[4]:
+                print(row)
+
+        db.close()
+    else:
+        pass
+
+if __name__ == '__main__':
+    filterEstates()
