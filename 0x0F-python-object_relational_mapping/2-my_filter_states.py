cdc57c405711874163a98415c74f62f5ea67873c
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-08-08 21:55:53 +0000

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+""" Filter states """
+import MySQLdb
+from sys import argv
+
+
+def filterEstates():
+    """  MySQLdb. """
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+
+    cur.execute('SELECT * FROM states WHERE name ="{}" ORDER\
+                BY id ASC'.format(argv[4]))
+    states = cur.fetchall()
+    for row in states:
+        print(row)
+
+    db.close()
+
+if __name__ == '__main__':
+    filterEstates()
