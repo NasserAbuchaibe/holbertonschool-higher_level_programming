5cb01ad6ef33b8331e2a266f31b0e4d9492cc036
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-08-08 23:08:01 +0000

    fix format

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,8 +10,8 @@ def filterEstates():
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
 
-    cur.execute('SELECT * FROM states WHERE name ="{}" ORDER\
-                BY id ASC'.format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name =%s ORDER\
+                BY id ASC", (argv[4],))
     states = cur.fetchall()
     for row in states:
         if row[1] == argv[4]:
