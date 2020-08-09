2b727e32fea016c8c74c8b7515a8a28670ced09a
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-08-09 23:10:36 +0000

    8 firts state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" ok """
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    sta =session.query(State).order_by(State.id).first()
+    print("{}: {}".format(sta.id, sta.name))
