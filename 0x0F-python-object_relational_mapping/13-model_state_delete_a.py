1b64af6ead7cba97439823942228647615bc00eb
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-08-10 01:35:35 +0000

    delete state with 'a' letter

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+""" ok """
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    fil = session.query(State).filter(State.name.contains('a'))
+    for sta in fil:
+        session.delete(sta)
+    session.commit()
+    session.close()
