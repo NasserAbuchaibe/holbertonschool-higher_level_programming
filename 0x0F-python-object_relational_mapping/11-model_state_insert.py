9db859a7b10c198a8eb4fb83a45aaf3f3c621503
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-08-10 01:01:43 +0000

    11 add state

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+""" ok """
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.add_all([State(name="Louisiana")])
+    session.commit()
+    id_v = session.query(State).filter_by(name="Louisiana").first()
+    if id_v:
+        print("{}".format(id_v.id))
