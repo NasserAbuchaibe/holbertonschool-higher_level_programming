a0338c229e84d6ab5eba5c7c3b2af691dcab3c22
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-08-10 01:21:35 +0000

    change name of object of State

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+""" ok """
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    sta = session.query(State).filter_by(id=2).first()
+    sta.name = "New Mexico"
+
+    session.commit()
+    session.close()
