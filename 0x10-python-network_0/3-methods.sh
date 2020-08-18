f97bd6850562ad610daf7e6cf38fb6f5ab8a26ad
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-08-18 20:34:10 +0000

    fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays
 # all HTTP methods the server will accept
-curl -sIX OPTIONS "$1" | grep Allow | cut -d ' ' -f2-
+curl -sI -X OPTIONS "$1" | grep Allow | cut -d ' ' -f2-
