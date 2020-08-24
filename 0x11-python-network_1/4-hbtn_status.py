0c5e38f954a2f6d8ef3b513115f59afc824c9560
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-08-24 21:13:55 +0000

    fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     """
     r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(r.text))
+    print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
