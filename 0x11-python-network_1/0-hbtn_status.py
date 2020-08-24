aa69de5511bdaf8cf3585915c62ec784024865e5
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-08-24 18:24:41 +0000

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Python script that fetches https://intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+message = """Body response:
+    - type: {}
+    - content: {}
+    - utf8 content: {}""".format(type(html), html, html.decode("utf-8"))
+print(message)
