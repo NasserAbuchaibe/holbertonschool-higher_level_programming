1ecef1a04b85136703c20c63162481af9b797566
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-08-24 18:26:34 +0000

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,12 @@
 import urllib.request
 
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-message = """Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}""".format(type(html), html, html.decode("utf-8"))
-print(message)
+if __name__ == "__main__":
+    
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+    message = """Body response:
+        - type: {}
+        - content: {}
+        - utf8 content: {}""".format(type(html), html, html.decode("utf-8"))
+    print(message)
