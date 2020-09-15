649aa3069a94615809d9e57a988674890d46cc3b
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-09-15 21:06:56 +0000

    fix variable

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,6 +4,8 @@ const lenArgv = process.argv.length;
 
 if (lenArgv === 2) {
   console.log('No argument');
-} else if (lenArgv >= 3) {
+} else if (lenArgv === 3) {
+  console.log('Argument found');
+} else if (lenArgv > 3) {
   console.log('Argument found');
 }
