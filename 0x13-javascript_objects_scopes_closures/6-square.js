c2f7b9d193925e5795b94bbfc5311b5737be541f
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-09-21 20:22:40 +0000

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const Squ = require('./5-square.js');
+
+class Square extends Squ {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('C'.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
