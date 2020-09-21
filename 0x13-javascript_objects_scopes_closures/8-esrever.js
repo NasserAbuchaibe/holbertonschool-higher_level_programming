1b6e416e56c7c38abf736219da04800e01bb8e05
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-09-21 21:03:36 +0000

    8-esrever.js fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,9 +3,8 @@
 exports.esrever = function (list) {
   const reverseList = [];
   let rev = 0;
-  for (let count = list.length - 1; count > 0; count--) {
+  for (let count = list.length - 1; count >= 0; count--, rev++) {
     reverseList[rev] = list[count];
-    rev++;
   }
   return reverseList;
 };
