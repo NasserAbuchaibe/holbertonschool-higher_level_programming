e8150571c8a1a6052a6b64fba9e7e65df9ab2c6b
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-09-22 18:16:37 +0000

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (data) {
-    console.log(data);
-  } else {
+  if (err) {
     console.log(err);
+  } else {
+    console.log(data);
   }
 });
